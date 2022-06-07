e9784504f490ae67b4fd49011a529d77b916246c
GOD0H <92855471+GOD0H@users.noreply.github.com>
2022-06-07 22:07:54 +0800

    pull 1.0

--- src/com/mono/Test.java
+++ src/com/mono/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("hello hot-fix!!");
         System.out.println("hot-fix!!!");
         System.out.println("master!!!!!");
+        System.out.println("pull   !!!!!");
     }
 }
